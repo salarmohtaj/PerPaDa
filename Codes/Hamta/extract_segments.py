83cc1f438b7cd1dcb4e11cd2683c77d3181ba496
Salar <salar.mohtaj@gmail.com>
2022-01-15 01:13:23 +0100

    extract fragments from the Hamta corpus

--- Codes/Hamta/extract_segments.py
+++ Codes/Hamta/extract_segments.py
@@ -6,8 +6,11 @@ import pickle
 list_of_fragments = []
 
 #### The Hamta corpus should be located in the same directory
+#### The data could be downloaded from following link
+#### http://ictrc.ac.ir/plagdet/PersianPlagdet2016-text-alignment-corpus.rar
+
 xml_files = glob.glob('hamta/04-simulated-obfuscation/*.xml')
-####
+
 for files in xml_files:
     tree = ET.parse(files)
     root = tree.getroot()
