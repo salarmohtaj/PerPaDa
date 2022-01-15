054f98f8e35e4175c1e15967157e660fbbf2b859
Salar <salar.mohtaj@gmail.com>
2022-01-15 01:06:18 +0100

    extract fragments from the Hamta corpus

---
+++ Codes/Hamta/extract_segments.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+import glob
+import pickle
+
+
+list_of_fragments = []
+
+#### The Hamta corpus should be located in the same directory
+xml_files = glob.glob('hamta/04-simulated-obfuscation/*.xml')
+####
+for files in xml_files:
+    tree = ET.parse(files)
+    root = tree.getroot()
+    sus_file = root.attrib["reference"]
+    for child in root:
+        sus_start = int(child.attrib["this_offset"])
+        sus_end = sus_start + int(child.attrib["this_length"])
+        source_file = child.attrib["source_reference"]
+        source_start = int(child.attrib["source_offset"])
+        source_end = source_start + int(child.attrib["source_length"])
+        with open("src/"+source_file,'r') as f:
+            source_text = f.read()
+        with open("susp/"+sus_file,'r') as f:
+            sus_text = f.read()
+        list_of_fragments.append((source_text[source_start:source_end+1],sus_text[sus_start:sus_end+1]))
+
+with open("list_of_fragments.LIST", 'wb') as f:
+    pickle.dump(list_of_fragments, f)
+
